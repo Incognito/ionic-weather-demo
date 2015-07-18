3d962e0717bcfec2b8870f8df61a418a50ae9853
Brian Graham <drakain@gmail.com>
2015-07-18 12:23:03 -0400

    Queries to openweathermap should return async

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('SevenDayCtrl', function($scope) {
-
+.controller('SevenDayCtrl', function($scope, WeatherClient) {
+  $scope.city = 'Toronto'
+  var forecast = WeatherClient.getWeeklyForecastByCity($scope.city);
+  forecast.then(function resolve(){
+    
+  });
 })
 
-.controller('FancySevenDayCtrl', function($scope, Chats) {
+.controller('FancySevenDayCtrl', function($scope, WeatherClient) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -12,10 +16,14 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  var forecast = WeatherClient.getWeeklyForecastByCity($scope.city);
+  forecast.then(function resolve(){
+    
+  });
 
 })
 
-.controller('PressureCtrl', function($scope) {
+.controller('PressureCtrl', function($scope, WeatherClient) {
   $scope.settings = {
   };
 });
